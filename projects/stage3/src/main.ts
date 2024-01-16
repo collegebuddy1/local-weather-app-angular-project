f1be0d22e1575881b00580a78b9c15e199862377
ritik-prog <ritikmakhija2002@gmail.com>
2024-01-16 11:28:05 +0530

    Initial commit

---
+++ projects/stage3/src/main.ts
@@ -0,0 +1,14 @@
+import { provideHttpClient } from '@angular/common/http'
+import { enableProdMode } from '@angular/core'
+import { bootstrapApplication } from '@angular/platform-browser'
+
+import { AppComponent } from './app/app.component'
+import { environment } from './environments/environment'
+
+if (environment.production) {
+  enableProdMode()
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [provideHttpClient()],
+}).catch((err) => console.error(err))
